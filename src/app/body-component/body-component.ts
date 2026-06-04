266ff874a3bdef5b05a590e76a7a9a908cec7958
leonamlucius <leonam253@gmail.com>
2026-06-04 17:55:58 -0300

    dados mockados

--- src/app/body-component/body-component.ts
+++ src/app/body-component/body-component.ts
@@ -1,9 +1,59 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-body-component',
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './body-component.html',
   styleUrls: ['./body-component.scss'],
 })
-export class BodyComponent {}
+export class BodyComponent {
+
+  public mockUp = [
+    {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+      sendBy: "User"
+    },
+     {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+      sendBy: "Bot"
+    },
+
+    {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+      sendBy: "User"
+    },
+     {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+      sendBy: "Bot"
+    },
+    {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+      sendBy: "User"
+    },
+     {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+      sendBy: "Bot"
+    },
+
+    {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+      sendBy: "User"
+    },
+     {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+      sendBy: "Bot"
+    },
+
+    {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+      sendBy: "User"
+    },
+     {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+      sendBy: "Bot"
+    },
+    
+
+  ]
+}
