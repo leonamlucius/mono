27a1037780f2c8bc6d2c069b282cec449022972c
leonamlucius <leonam253@gmail.com>
2026-06-09 10:14:15 -0300

    icones de info

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,12 +4,16 @@ import { RouterOutlet, ɵEmptyOutletComponent } from '@angular/router';
 import {BodyComponent} from "./body-component/body-component";
 import {InputComponent} from "./input-component/input-component";
 import { Subject } from 'rxjs';
-
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { featherAirplay } from '@ng-icons/feather-icons';
+import { heroUsers } from '@ng-icons/heroicons/outline';
+import { bootstrapLinkedin, bootstrapGithub } from '@ng-icons/bootstrap-icons';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BodyComponent, InputComponent, ɵEmptyOutletComponent, NgIf, NgClass],
+  imports: [RouterOutlet, BodyComponent, InputComponent, ɵEmptyOutletComponent, NgIf, NgClass, NgIcon],
   templateUrl: './app.html',
-  styleUrls: ['./app.scss']
+  styleUrls: ['./app.scss'],
+  providers: [provideIcons({ featherAirplay, heroUsers, bootstrapLinkedin, bootstrapGithub })]
 })
 export class App {
   protected readonly title = signal('mono');
@@ -27,5 +31,13 @@ export class App {
     }
     console.log('Iniciar');
     this.isInitialized.set(true);
+  }
+
+  public goTolinkedin(): void {
+    window.open('https://www.linkedin.com/in/leonamlucius/', '_blank');
+  }
+
+    public goToGithub(): void {
+    window.open('https://github.com/leonamlucius', '_blank');
   }
 }
