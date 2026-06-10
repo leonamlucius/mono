676bbbae20062e0d146146b2d131d459b3aeb85a
leonamlucius <leonam253@gmail.com>
2026-06-10 19:12:55 -0300

    Merge pasta mono-api da branch mono-api

---
+++ mono-api/src/test/java/com/mono/monoapi/MonoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mono.monoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
