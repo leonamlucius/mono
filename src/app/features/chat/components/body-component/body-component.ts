d5a3295b27a07e7ffa830845570e863c8ecf1256
leonamlucius <leonam253@gmail.com>
2026-08-17 19:09:34 -0300

    [UPDATE] Novas texto de boa-vinda

--- src/app/features/chat/components/body-component/body-component.ts
+++ src/app/features/chat/components/body-component/body-component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, signal, inject, ViewChild } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf, NgClass } from '@angular/common';
 import { trigger, transition, style, animate } from '@angular/animations';
 import { TextToSpeechService } from '../../../../core/services/text-to-speech.service';
 import { MarkdownPipe } from './markdown.pipe';
@@ -9,7 +9,7 @@ import removeMarkdown from 'remove-markdown';
 
 @Component({
   selector: 'app-body-component',
-  imports: [NgFor, NgIf, MarkdownPipe, WarningComponent],
+  imports: [NgFor, NgIf, MarkdownPipe, WarningComponent, NgClass],
   templateUrl: './body-component.html',
   styleUrls: ['./body-component.scss'],
   animations: [
@@ -48,26 +48,26 @@ export class BodyComponent {
 
   public texts = [
     {
-      text: 'Faça sua pergunta, dúvidas, curiosidades e conversas com o Mono!',
+      text: 'Faça suas perguntas e conservas com o mono!',
       icon: `<span class="material-symbols-outlined">
         question_mark
         </span>`,
     },
     {
-      text: 'O Mono é um assistente virtual inteligente, projetado para fornecer respostas rápidas e precisas às suas perguntas. Ele utiliza tecnologia avançada de processamento de linguagem natural para entender suas consultas e oferecer informações relevantes de forma eficiente.',
+      text: 'O Mono é um assistente virtual inteligente, projetado para fornecer respostas rápidas.',
       icon: `<span class="material-symbols-outlined">
         flash_on
         </span>`,
     },
     {
-      text: 'Com o Mono, você pode obter respostas para uma ampla variedade de tópicos, desde informações gerais até questões específicas. Ele é capaz de compreender o contexto das suas perguntas e fornecer respostas personalizadas, tornando a interação mais fluida e satisfatória.',
+      text: 'Com o Mono, você pode obter respostas para uma ampla variedade de tópicos.',
 
       icon: `<span class="material-symbols-outlined">
       emoji_objects
       </span>`,
     },
     {
-      text: 'O Mono é uma ferramenta útil para quem busca informações rápidas e confiáveis, seja para resolver dúvidas do dia a dia, obter insights sobre um assunto específico ou simplesmente ter uma conversa interessante. Experimente o Mono e descubra como ele pode facilitar sua vida com respostas inteligentes e eficientes!',
+      text: 'O Mono é uma ferramenta útil para quem busca informações rápidas e confiáveis.',
 
       icon: `<span class="material-symbols-outlined">
         mood
@@ -76,16 +76,16 @@ export class BodyComponent {
   ];
 
   ngOnInit() {
-    this.textoAtual.set(this.texts[0].text);
-    this.iconAtual.set(this.texts[0].icon);
-    setInterval(() => {
-      this.textoAtual.set('');
-      setTimeout(() => {
-        this.indice = (this.indice + 1) % this.texts.length;
-        this.textoAtual.set(this.texts[this.indice].text);
-        this.iconAtual.set(this.texts[this.indice].icon);
-      }, 50);
-    }, 8000);
+    // this.textoAtual.set(this.texts[0].text);
+    // this.iconAtual.set(this.texts[0].icon);
+    // setInterval(() => {
+    //   this.textoAtual.set('');
+    //   setTimeout(() => {
+    //     this.indice = (this.indice + 1) % this.texts.length;
+    //     this.textoAtual.set(this.texts[this.indice].text);
+    //     this.iconAtual.set(this.texts[this.indice].icon);
+    //   }, 50);
+    // }, 8000);
 
     this.adjustTextAlignment();
   }
