6f1fbc590183ba8143db0a235b1eab6d770a9961
leonamlucius <leonam253@gmail.com>
2026-08-20 16:00:10 -0300

    [UPDATE] Melhorias no modal de warning

--- src/app/shared/components/warning-component/warning-component.ts
+++ src/app/shared/components/warning-component/warning-component.ts
@@ -14,6 +14,32 @@ export class WarningComponent {
 
   public showModalWarning = signal(false);
 
+  private startY = 0;
+
+  private readonly minSwipeDistance = 30;
+
+  private openTimer: any;
+
+  private closeTimer: any;
+
+  public onTouchStart(event: TouchEvent): void {
+    const isMobile = window.innerWidth <= 768;
+
+    if (!isMobile) {
+      return;
+    }
+    this.startY = event.touches[0].clientY;
+  }
+
+  public onTouchEnd(event: TouchEvent): void {
+    const endY = event.changedTouches[0].clientY;
+    const distance = endY - this.startY;
+
+    if (distance > this.minSwipeDistance) {
+      this.closeModal();
+    }
+  }
+
   public openModal(text: string): void {
     if (this.showModalWarning()) {
       return;
@@ -22,19 +48,31 @@ export class WarningComponent {
     this.modalWarningText = text;
     this.showModalWarning.set(true);
 
-    setTimeout(() => {
+    this.openTimer = setTimeout(() => {
       this.isClosingModal.set(true);
-      setTimeout(() => {
+      this.closeTimer = setTimeout(() => {
         this.showModalWarning.set(false);
         this.isClosingModal.set(false);
       }, 500);
     }, 2000);
   }
   public closeModal(): void {
+    this.clearTimer();
     this.isClosingModal.set(true);
-    setTimeout(() => {
+    this.closeTimer = setTimeout(() => {
       this.showModalWarning.set(false);
       this.isClosingModal.set(false);
     }, 500);
+  }
+
+  public clearTimer(): void {
+    if (this.openTimer) {
+      clearTimeout(this.openTimer);
+      this.openTimer = null;
+    }
+    if (this.closeTimer) {
+      clearTimeout(this.closeTimer);
+      this.closeTimer = null;
+    }
   }
 }
