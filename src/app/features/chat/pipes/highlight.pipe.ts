6df7ca35341ad10516cc611a93885dc7f37ff3f8
leonamlucius <leonam253@gmail.com>
2026-08-21 15:37:22 -0300

    [FIX] Resolvido erro no pipe de marcação

--- src/app/features/chat/pipes/highlight.pipe.ts
+++ src/app/features/chat/pipes/highlight.pipe.ts
@@ -1,22 +1,31 @@
-
 import { Pipe, PipeTransform, inject } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
   name: 'highlight',
-  standalone: true
+  standalone: true,
 })
 export class HighlightPipe implements PipeTransform {
   private sanitizer = inject(DomSanitizer);
 
-  transform(htmlContent: SafeHtml | string | null | undefined, searchTerm: string | null | undefined): SafeHtml {
+  transform(
+    htmlContent: SafeHtml,
+    searchTerm: string | null | undefined
+  ): SafeHtml {
     if (!htmlContent) return '';
+
     if (!searchTerm || !searchTerm.trim()) return htmlContent;
 
+    console.log('HighlightPipe: htmlContent:', htmlContent);
     const escaped = searchTerm.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-   
+
+    let safeContent = htmlContent as any;
+
+    let contentString =
+      safeContent.changingThisBreaksApplicationSecurity as string;
+
     const regex = new RegExp(`(${escaped})(?![^<]*>)`, 'gi');
-    const highlighted = (htmlContent as string).replace(regex, '<mark>$1</mark>');
+    const highlighted = contentString.replace(regex, '<mark>$1</mark>');
 
     return this.sanitizer.bypassSecurityTrustHtml(highlighted);
   }
